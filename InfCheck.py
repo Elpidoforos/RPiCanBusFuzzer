6df46507dba130c6eddf0d2e3bb9b79db2fda619
Elpidoforos Arapantonis <elpidoforos.arapantonis@volvocars.com>
2019-05-29 09:21:15 +0200

    Fixes in the InfCheck class

--- InfCheck.py
+++ InfCheck.py
@@ -10,13 +10,13 @@ class InfCheck:
     def inf_status(self, inf_name):
         self.bus = ""
         # Return 1 upon error, and 0 upon succes
-        self.inf_name = subprocess.call(
+        self.inf_name_check = subprocess.call(
             ["ifconfig", self.inf_name], stdout=open(os.devnull, 'wb'))
         # Check if the interface name exists
-        if self.inf_name == 1:
+        if self.inf_name_check == 1:
             print("ERROR: Wrong interface name, please check the CAN Bus interface name and restart the application.\n")
             exit()
-        elif self.inf_name == 0:
+        elif self.inf_name_check == 0:
             # Check if the interface is up"
             self.get_can_inf = "ip a show " + self.inf_name
             process = subprocess.Popen(
